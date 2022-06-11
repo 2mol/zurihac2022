90523a52e210206a77ce477d17b086ff65a7ed1b
2mol <2mol@users.noreply.github.com>
2022-06-11 17:20:15 +0200

    pokeymon

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,26 @@
 module Main where
 
+import Data.Text (Text)
+
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+data Energy
+    = Colorless | Grass | Fire | Water | Lightning
+    | Fighting | Psychic | Darkness | Metal | Dragon
+
+type Natural = Int
+
+data Attack = Attack
+    { attackName :: Text
+    , cost :: [Energy]
+    , damage :: Natural
+    }
+
+data Card = Card
+    { name :: Text
+    , typ :: Energy
+    , hp :: Natural
+    , attacks :: [Attack]
+    }
